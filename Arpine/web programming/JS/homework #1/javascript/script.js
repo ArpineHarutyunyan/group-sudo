f8ba62f81fa5be143cc91d19f093dd10606096bf
Linux233333 <studentinsta0@gmail.com>
2018-06-25 16:23:52 +0400

    homework

--- Arpine/web programming/JS/homework #1/javascript/script.js
+++ Arpine/web programming/JS/homework #1/javascript/script.js
@@ -15,9 +15,23 @@ $(function() {
 
         return false;
     });
+
+    $("#headform").submit(function() {
+        pushResult();
+        return false;
+    });
 });
 
 function push() {
     $("#hname").html($("#name").val());
     $("#hlname").html($("#lname").val());
+}
+
+function pushResult() {
+    $("#name2").html($("#name1").val());
+    $("#lastname1").html($("#lastname").val());
+    $("#address1").html($("#address").val());
+    $("#compn1").html($("#compn").val());
+    $("#phone1").html($("#phone").val());
+    $("#mail1").html($("#mail").val());
 }
