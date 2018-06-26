82bcf3716d2b5b9789dd024df7eab5501f2d6f4a
ArpineHarutyunyan <arpi.sun@mail.ru>
2018-06-26 13:54:40 +0400

    change homework

--- Arpine/web programming/JS/homework #1/javascript/script.js
+++ Arpine/web programming/JS/homework #1/javascript/script.js
@@ -1,5 +1,13 @@
 $(function() {
-    $("#modalForm").modal("show");
+    if (localStorage.getItem(`js-result`)) {
+        $(`#container_cart`).html(localStorage.getItem(`js-result`));
+    }
+
+    if (localStorage.getItem(`header_result`)) {
+        $(`#header-result`).html(localStorage.getItem(`header_result`));
+    } else {
+        $("#modalForm").modal("show");
+    }
 
     $("#formLogin").submit(function() {
         push();
@@ -12,7 +20,7 @@ $(function() {
         if ($('#female').is(':checked')) {
             $("#checkGender").html("Hi Mrs.");
         }
-
+        save();
         return false;
     });
 
@@ -28,16 +36,21 @@ function push() {
 }
 
 function info_cart(form_data) {
-    var table ="<table ><tr><td> Name: </td> <td>";
+    var table = "<table ><tr><td> Name: </td> <td>";
     table += form_data["name"].value +
-            "</td></tr><tr><td>Surname: </td> <td>"+ form_data["surname"].value +
-            "</td></tr><tr><td>Adress: </td> <td>"+ form_data["adress"].value +
-            "</td></tr><tr><td>Country: </td> <td>"+ form_data["country"].value +
-            "</td></tr><tr><td>Phone: </td> <td>"+ form_data["phone"].value + 
-            "</td></tr><tr><td>E-mail: </td> <td>"+ form_data["email"].value + 
-            "</td></tr> </table>";
+        "</td></tr><tr><td>Surname: </td> <td>" + form_data["surname"].value +
+        "</td></tr><tr><td>Adress: </td> <td>" + form_data["adress"].value +
+        "</td></tr><tr><td>Country: </td> <td>" + form_data["country"].value +
+        "</td></tr><tr><td>Phone: </td> <td>" + form_data["phone"].value +
+        "</td></tr><tr><td>E-mail: </td> <td>" + form_data["email"].value +
+        "</td></tr> </table>";
     document.getElementById("container_cart").innerHTML += table;
     for (var i = 0; i < 6; i++) {
-        form_data[i].value = "";  
+        form_data[i].value = "";
     };
+    localStorage.setItem(`js-result`, $(`#container_cart`).html());
+}
+
+function save() {
+    localStorage.setItem(`header_result`, $(`#header-result`).html());
 }
